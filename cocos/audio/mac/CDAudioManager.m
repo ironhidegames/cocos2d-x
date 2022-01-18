18666b1a2f5d0f071064cd6c7f70d167c3aa676a
Guillermo Amorin <guillermo@ironhidegames.com>
2022-01-18 12:35:55 -0300

    fix compilation for mac on Xcode 12+

--- cocos/audio/mac/CDAudioManager.m
+++ cocos/audio/mac/CDAudioManager.m
@@ -325,8 +325,9 @@ static BOOL configured = FALSE;
 
 -(BOOL) isOtherAudioPlaying {
     UInt32 isPlaying = 0;
-    UInt32 varSize = sizeof(isPlaying);
-    AudioSessionGetProperty (kAudioSessionProperty_OtherAudioIsPlaying, &varSize, &isPlaying);
+//    UInt32 varSize = sizeof(isPlaying);
+//    AudioSessionGetProperty (kAudioSessionProperty_OtherAudioIsPlaying, &varSize, &isPlaying);
+    isPlaying = [[AVAudioSession sharedInstance] isOtherAudioPlaying];
     return (isPlaying != 0);
 }
 
@@ -405,8 +406,8 @@ static BOOL configured = FALSE;
     if ((self = [super init])) {
         
         //Initialise the audio session 
-        AVAudioSession* session = [AVAudioSession sharedInstance];
-        session.delegate = self;
+//        AVAudioSession* session = [AVAudioSession sharedInstance];
+//        session.delegate = self;
     
         _mode = mode;
         backgroundMusicCompletionSelector = nil;
@@ -479,28 +480,8 @@ static BOOL configured = FALSE;
 #if TARGET_IPHONE_SIMULATOR
     //Calling audio route stuff on the simulator causes problems
     return NO;
-#else    
-    CFStringRef newAudioRoute;
-    UInt32 propertySize = sizeof (CFStringRef);
-    
-    AudioSessionGetProperty (
-                             kAudioSessionProperty_AudioRoute,
-                             &propertySize,
-                             &newAudioRoute
-                             );
-    
-    if (newAudioRoute == NULL) {
-        //Don't expect this to happen but playing safe otherwise a null in the CFStringCompare will cause a crash
-        return YES;
-    } else {    
-        CFComparisonResult newDeviceIsMuted =    CFStringCompare (
-                                                                 newAudioRoute,
-                                                                 (CFStringRef) @"",
-                                                                 0
-                                                                 );
-        
-        return (newDeviceIsMuted == kCFCompareEqualTo);
-    }    
+#else
+    return NO;
 #endif
 }    
 
